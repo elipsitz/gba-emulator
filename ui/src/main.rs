62933af0971dd10e0d3c5ef74f84c6e5de0b975a
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-12-21 09:21:08 -0800

    Use minifb to create a window

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -1,4 +1,10 @@
-fn main() {
+const WIDTH: usize = 240;
+const HEIGHT: usize = 160;
+
+use gba_core::Gba;
+use minifb::{Key, Window, WindowOptions};
+
+fn make_gba() -> Gba {
     let args = std::env::args().collect::<Vec<_>>();
     if args.len() != 2 {
         eprintln!("Usage: gba <path to rom>");
@@ -16,5 +22,41 @@ fn main() {
 
     let mut gba = gba_core::Gba::new(bios.into(), rom);
     gba.skip_bios();
+    gba
+}
+
+fn main() {
+    // Create the gba.
+    let mut gba = make_gba();
+
+    // Create the window.
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
+    let window_options = WindowOptions {
+        scale: minifb::Scale::X2,
+        scale_mode: minifb::ScaleMode::Stretch,
+        ..WindowOptions::default()
+    };
+    let mut window =
+        Window::new("GBA", WIDTH, HEIGHT, window_options).expect("Failed to create window.");
+    // Limit to ~60 FPS.
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    loop {
+        if !window.is_open() || window.is_key_down(Key::Escape) {
+            // User wants to exit.
+            break;
+        }
+
+        // TODO: get input.
+
+        // TODO: run emulator for a frame.
+
+        // TODO: update buffer with data from emulator.
+        for i in buffer.iter_mut() {
+            *i = 0x00FF7733; // write something more funny here!
+        }
+        window.update_with_buffer(&buffer, WIDTH, HEIGHT).unwrap();
+    }
+
     gba.hack_run();
 }
