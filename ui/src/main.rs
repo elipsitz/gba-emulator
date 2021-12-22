22304d7b9b946a702f798d793c67dc97e6e9f7ec
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-12-21 16:37:40 -0800

    Add FPS counter

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -3,6 +3,7 @@ const HEIGHT: usize = 160;
 
 use gba_core::Gba;
 use minifb::{Key, Window, WindowOptions};
+use std::time::{Duration, Instant};
 
 fn make_gba() -> Gba {
     let args = std::env::args().collect::<Vec<_>>();
@@ -40,6 +41,8 @@ fn main() {
     // Limit to ~60 FPS.
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
 
+    let mut frame_counter = 0;
+    let mut last_fps_update = Instant::now();
     loop {
         if !window.is_open() || window.is_key_down(Key::Escape) {
             // User wants to exit.
@@ -50,11 +53,22 @@ fn main() {
 
         // Run emulator for a frame.
         gba.emulate_frame();
+        frame_counter += 1;
 
         // Update window with the framebuffer.
         let framebuffer = gba.framebuffer();
         window
             .update_with_buffer(framebuffer, WIDTH, HEIGHT)
             .unwrap();
+
+        // Update FPS counter.
+        let elapsed = Instant::now() - last_fps_update;
+        if elapsed >= Duration::from_secs(1) {
+            let fps = (frame_counter as f64) / elapsed.as_secs_f64();
+            let fps = fps.round() as i32;
+            window.set_title(&format!("GBA (FPS: {})", fps));
+            frame_counter = 0;
+            last_fps_update = Instant::now();
+        }
     }
 }
