a318f43c4137ea5de333749be6093040503ab2ac
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-12-26 17:34:45 -0800

    bugfix: Handle empty rlist in Thumb multiple load/store

--- gba_core/src/cpu/thumb.rs
+++ gba_core/src/cpu/thumb.rs
@@ -462,6 +462,9 @@ fn thumb_exec_push_pop<const POP: bool, const PC_LR: bool>(
         }
     }
 
+    if POP {
+        s.cpu_internal_cycle();
+    }
     s.cpu.next_fetch_access = NonSequential;
     s.cpu_reg_set(REG_SP, new_sp);
     instruction_result
@@ -474,36 +477,57 @@ fn thumb_exec_ldr_str_multiple<const LOAD: bool>(s: &mut Gba, inst: u16) -> Inst
     let num_registers = reg_list.count_ones();
 
     let start_address = s.cpu_reg_get(reg_n);
-    let new_address = start_address + (4 * num_registers);
+    let mut new_address = start_address + (4 * num_registers);
 
-    let mut address = start_address;
-    let mut access_type = NonSequential;
-    let mut first = true;
-    for reg in 0..=7 {
-        if reg_list.bit(reg) {
-            if LOAD {
-                let value = s.cpu_load32(address, access_type);
-                s.cpu_reg_set(reg, value);
-            } else {
-                let mut value = s.cpu_reg_get(reg);
-                if reg == reg_n && !first {
-                    // Storing the address base register. Store the *new* value.
-                    value = new_address;
+    let instruction_result = if num_registers != 0 {
+        let mut address = start_address;
+        let mut access_type = NonSequential;
+        let mut first = true;
+        for reg in 0..=7 {
+            if reg_list.bit(reg) {
+                if LOAD {
+                    let value = s.cpu_load32(address, access_type);
+                    s.cpu_reg_set(reg, value);
+                } else {
+                    let mut value = s.cpu_reg_get(reg);
+                    if reg == reg_n && !first {
+                        // Storing the address base register. Store the *new* value.
+                        value = new_address;
+                    }
+                    s.cpu_store32(address & !0b11, value, access_type);
                 }
-                s.cpu_store32(address & !0b11, value, access_type);
+                first = false;
+                address += 4;
+                access_type = Sequential;
             }
-            first = false;
-            address += 4;
-            access_type = Sequential;
         }
-    }
+        InstructionResult::Normal
+    } else {
+        // Weird behavior ("unpredictable" according to the ARM ARM).
+        // If the list is empty, it'll transfer r15 only, but decrement/increment as if
+        // all registers were transferred.
+        new_address = start_address + 0x40;
+        if LOAD {
+            let value = s.cpu_load32(start_address, NonSequential);
+            s.cpu_reg_set(REG_PC, value);
+            InstructionResult::Branch
+        } else {
+            // When storing here we actually store PC + 6... so + 2 from what we read here.
+            let value = s.cpu_reg_get(REG_PC) + 2;
+            s.cpu_store32(start_address & !0b11, value, NonSequential);
+            InstructionResult::Normal
+        }
+    };
 
+    if LOAD {
+        s.cpu_internal_cycle();
+    }
     s.cpu.next_fetch_access = NonSequential;
     if !(LOAD && reg_list.bit(reg_n)) {
         // With LOAD, if Rn is in Rlist, the new address is not written back.
         s.cpu_reg_set(reg_n, new_address);
     }
-    InstructionResult::Normal
+    instruction_result
 }
 
 // THUMB.16: conditional branch
